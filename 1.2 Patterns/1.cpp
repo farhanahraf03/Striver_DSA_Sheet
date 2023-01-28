9a3163a75df5aea35e2c59c44adc8aae9f11a9d7
Farhan Ansari <farhanahraf03@gmail.com>
2023-01-29 00:22:46 +0530

    Update and rename 1 to 1.cpp

---
+++ 1.2 Patterns/1.cpp
@@ -0,0 +1,21 @@
+void printSquare(int n) {
+   
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+}
+    
+/*
+Output
+n=5
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+*/
