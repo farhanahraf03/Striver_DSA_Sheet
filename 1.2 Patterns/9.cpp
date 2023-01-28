6e86dc1fe20744387024479ba139608a860ac137
Farhan Ansari <farhanahraf03@gmail.com>
2023-01-29 01:07:29 +0530

    Create 9.cpp

---
+++ 1.2 Patterns/9.cpp
@@ -0,0 +1,56 @@
+void printDiamond(int n) {
+        
+        
+        for(int i=1; i<=n; i++)
+        {
+            //left spaces
+            for(int j=1; j<=n-i; j++)
+                cout<<" ";
+                
+            //stars
+            for(int j=1; j<2*i; j++)
+                cout<<"*";
+            
+            //right spaces
+            for(int j=1; j<=n-i; j++)
+                cout<<" ";
+                
+            cout<<endl;
+        }
+    
+	    
+	    for(int i=1; i<=n; i++)
+	    {
+	        
+	        //left blanks
+	        for(int j=1; j<i; j++)
+	            cout<<" ";
+	            
+	        //stars
+	        for(int j=1; j<=2*(n-i)+1; j++)
+	            cout<<"*";
+	            
+	        //right blanks
+	        for(int j=1; j<i; j++)
+	            cout<<" ";
+	            
+	        cout<<endl;
+	    }
+	
+    }
+
+/*
+output -> n=5
+
+    *   
+   ***  
+  ***** 
+ *******
+*********
+*********
+ *******
+  ***** 
+   ***  
+    * 
+
+*/
